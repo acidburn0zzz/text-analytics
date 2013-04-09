cf57568fe860772bf6c5556d98998901aeb77d5d
Doug P <dougtko@gmail.com>
2013-04-09 13:51:02 -0400

    added categories method to Brains model

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -2,4 +2,17 @@ class Brain < ActiveRecord::Base
   # attr_accessible :title, :body
     attr_accessible :classifier_type, :name
     has_many :phrases
+    
+    def categories
+        categories = []
+        if self.classifier_type == 'LSI'
+            c = Marshal.load self.classifier
+            c.items.each do |item|
+                c.categories_for(item).each do |item|
+                    categories << item
+                end
+            end
+        end
+        return categories.uniq
+    end
 end
